9697f8793f2ffa8290f7ff8ef213a32ae0f33656
exteriorduke319 <yarnevictanner@gmail.com>
2023-09-27 09:37:35 -0400

    Init

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+        int birthMonth = 0;
+
+        System.out.print("Enter the number of your birth month: ");
+
+        if (scan.hasNextInt()) {
+            birthMonth = scan.nextInt();
+            if (birthMonth >= 1 && birthMonth <= 12) {
+                System.out.println("Your birth month is: " + birthMonth);
+            } else {
+                System.out.println("You entered an invalid value for birth month: " + birthMonth);
+                System.exit(0);
+            }
+        } else {
+            System.out.println("You entered an invalid value for birth month: " + birthMonth);
+            System.exit(0);
+        }
     }
 }
